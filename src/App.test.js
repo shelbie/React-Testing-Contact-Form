483b55aca7745cb4135db056b72242f9d94dd744
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-06-14 18:10:05 -0500

    int

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
+
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm, {register, errors} from "./components/ContactForm"
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+
+
+    describe("last name has value", () => {
+      test("lastname has value", () => {
+        expect(register("Taylor")).min(1)
+      })
+    })
